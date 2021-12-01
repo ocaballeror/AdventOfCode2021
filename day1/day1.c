04322f33cfbd34661c224b03659aeee27fc8328c
Oscar Caballero <oscar.caballero@bayes.gg>
2021-12-01 11:28:47 +0100

    Better day 1 part 2

--- day1/day1.c
+++ day1/day1.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void incr_window(int* window, int* window_count, int value) {
-    if(*window_count < 0) {}
-    else if(*window_count == 0) {
-        *window = value;
-    } else if(*window_count < 3) {
-        *window += value;
-    }
-
-    // cannot use mod here bc it would screw negative values
-    (*window_count)++;
-    if(*window_count > 3) {
-        *window_count = 0;
-    }
-}
-
 int part2(){
     FILE* fp = fopen("input", "r");
     if(!fp) {
@@ -24,24 +8,21 @@ int part2(){
         exit(1);
     }
     int count = 0;
-    int read;
-    int window1 = 0, window2 = 0, window3 = 0, window4 = 0;
-    int window1_count = 0, window2_count = -1, window3_count = -2, window4_count = -3;
+    int read, read_count = 0;
+    int window[3] = {0, 0, 0};
+    int window_sum = 0, new_window_sum;
     while(!feof(fp)) {
         fscanf(fp, "%d", &read);
-        incr_window(&window1, &window1_count, read);
-        incr_window(&window2, &window2_count, read);
-        incr_window(&window3, &window3_count, read);
-        incr_window(&window4, &window4_count, read);
-        if(window1_count == 0 && window2_count == 3 && window2 > window1)
+        read_count++;
+        new_window_sum = window_sum + read - window[0];
+        if(read_count > 3 && new_window_sum > window_sum) {
             count++;
-        else if(window2_count == 0 && window3_count == 3 && window3 > window2)
-            count++;
-        else if(window3_count == 0 && window4_count == 3 && window4 > window3)
-            count++;
-        else if(window4_count == 0 && window1_count == 3 && window1 > window4 && window4 > 0)
-            count++;
+        }
+        window_sum = new_window_sum;
 
+        window[0] = window[1];
+        window[1] = window[2];
+        window[2] = read;
     }
     fclose(fp);
     return count;
